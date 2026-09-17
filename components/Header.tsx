c6205a5de3d1c01ae8510cb4e9ffef25b14b6ae8
chithraksha16 <chithrakshakharvi@gmail.com>
2026-09-17 21:55:35 +0530

    added logo image

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Container from './Container'
 import Link from 'next/link'
+import Image from 'next/image'
 
 
 
@@ -23,7 +24,12 @@ return (
     
     <Container>
         <div className=' w-full h-18 px-5 flex justify-between'>
-        <h1>Logo</h1>
+        <Image
+        width={70} 
+        height={70}
+        className='rounded-full size-10'
+        src="/chithraksha logo.webp"
+         alt=""/>
             <div className='flex gap-10 font-sans font-normal text-[15px]'>
                 <div className='flex gap-10'>
                 {navItems.map((items,idx)=>(
